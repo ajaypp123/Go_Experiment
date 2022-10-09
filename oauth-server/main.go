08f7a876c61221d687dc4b72fe8e81c84744679a
Ajay Paratmandali <ajaypp123@gmail.com>
2022-10-09 19:21:08 +0530

    Add oauth server

---
+++ oauth-server/main.go
@@ -0,0 +1,104 @@
+/*
+
+https://pkg.go.dev/gopkg.in/oauth2.v3@v3.12.0
+
+
+
+http://localhost:9096/credentials
+
+http://localhost:9096/token?grant_type=client_credentials&client_id=83f20301&client_secret=dae69c1e&scope=all
+{
+"access_token": "J-7WM_BFN3ID9QBBTET1HQ",
+"expires_in": 7200,
+"scope": "all",
+"token_type": "Bearer"
+}
+
+
+http://localhost:9096/test?access_token=E7HLBNSCNX6JTK9ZLGZGIG
+J-7WM_BFN3ID9QBBTET1HQ
+
+http://localhost:9096/protected?access_token=NSFLLKLRNFCBXNNNI8PLLQ
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+	"gopkg.in/oauth2.v3/models"
+
+	"gopkg.in/oauth2.v3/errors"
+	"gopkg.in/oauth2.v3/manage"
+	"gopkg.in/oauth2.v3/server"
+	"gopkg.in/oauth2.v3/store"
+)
+
+func main() {
+	manager := manage.NewDefaultManager()
+	manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
+
+	// token memory store
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	// client memory store
+	clientStore := store.NewClientStore()
+
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewDefaultServer(manager)
+	srv.SetAllowGetAccessRequest(true)
+	srv.SetClientInfoHandler(server.ClientFormHandler)
+	manager.SetRefreshTokenCfg(manage.DefaultRefreshTokenCfg)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		srv.HandleTokenRequest(w, r)
+	})
+
+	http.HandleFunc("/credentials", func(w http.ResponseWriter, r *http.Request) {
+		clientId := uuid.New().String()[:8]
+		clientSecret := uuid.New().String()[:8]
+		err := clientStore.Set(clientId, &models.Client{
+			ID:     clientId,
+			Secret: clientSecret,
+			Domain: "http://localhost:9094",
+		})
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"CLIENT_ID": clientId, "CLIENT_SECRET": clientSecret})
+	})
+
+	http.HandleFunc("/protected", validateToken(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, I'm protected"))
+	}, srv))
+
+	log.Fatal(http.ListenAndServe(":9096", nil))
+}
+
+func validateToken(f http.HandlerFunc, srv *server.Server) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := srv.ValidationBearerToken(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		f.ServeHTTP(w, r)
+	})
+}
